18fa9f4236a0ebf87d223d89ab9a326c8f190097
Shevchik <Shev4ik.den@gmail.com>
2013-08-31 01:31:59 +0400

    Change decoder hashmap fill.
    It is not so creepy now.

--- src/anvilrenamefix/AnvilNameDecoder.java
+++ src/anvilrenamefix/AnvilNameDecoder.java
@@ -27,43 +27,20 @@ public class AnvilNameDecoder {
 		fillHashMap();
 	}
 	
-	//не стреляйте в пианиста, он играет как умеет (да нихрена, просто из пианино выдрали всё и заставляют играть на пустом ящике)
 	private HashMap<Byte, Character> bytesymborpair =  new HashMap<Byte, Character>();
 	private void fillHashMap()
 	{
 		//put lowercase chars
-		bytesymborpair.put((byte) -32,'а');
-		bytesymborpair.put((byte) -31,'б');
-		bytesymborpair.put((byte) -30,'в');
-		bytesymborpair.put((byte) -29,'г');
-		bytesymborpair.put((byte) -28,'д');
-		bytesymborpair.put((byte) -27,'е');
-		bytesymborpair.put((byte) -26,'ж');
-		bytesymborpair.put((byte) -25,'з');
-		bytesymborpair.put((byte) -24,'и');
-		bytesymborpair.put((byte) -23,'й');
-		bytesymborpair.put((byte) -22,'к');
-		bytesymborpair.put((byte) -21,'л');
-		bytesymborpair.put((byte) -20,'м');
-		bytesymborpair.put((byte) -19,'н');
-		bytesymborpair.put((byte) -18,'о');
-		bytesymborpair.put((byte) -17,'п');
-		bytesymborpair.put((byte) -16,'р');
-		bytesymborpair.put((byte) -15,'с');
-		bytesymborpair.put((byte) -14,'т');
-		bytesymborpair.put((byte) -13,'у');
-		bytesymborpair.put((byte) -12,'ф');
-		bytesymborpair.put((byte) -11,'х');
-		bytesymborpair.put((byte) -10,'ц');
-		bytesymborpair.put((byte) -9,'ч');
-		bytesymborpair.put((byte) -8,'ш');
-		bytesymborpair.put((byte) -7,'щ');
-		bytesymborpair.put((byte) -6,'ъ');
-		bytesymborpair.put((byte) -5,'ы');
-		bytesymborpair.put((byte) -4,'ь');
-		bytesymborpair.put((byte) -3,'э');
-		bytesymborpair.put((byte) -2,'ю');
-		bytesymborpair.put((byte) -1,'я');
+		int charvalue = Character.getNumericValue('а');
+		for (byte i = -32 ; i<-16; i++, charvalue++)
+		{
+			bytesymborpair.put(i,(char) charvalue);
+		}
+		charvalue = Character.getNumericValue('р');
+		for (byte i = -16 ; i<0; i++, charvalue++)
+		{
+			bytesymborpair.put(i,(char) charvalue);
+		}
 		//put uppercase chars
 		for (byte number : new HashSet<Byte>(bytesymborpair.keySet()))
 		{
